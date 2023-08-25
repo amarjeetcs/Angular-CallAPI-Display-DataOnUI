db11f6fb1bff410731415d4012598c10e12db01b
Rajnish <rajnishcs79@gmail.com>
2023-08-25 23:55:31 +0530

    API-Angular-Call-DisplayDataOnUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
-  imports: [ BrowserModule,AppRoutingModule,HttpClientModule],
-  providers: [],
+  imports: [ 
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule],
+  providers: [MatSnackBar],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
